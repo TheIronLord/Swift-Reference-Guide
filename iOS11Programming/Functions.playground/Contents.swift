170e4b26f2411e1d897021540ab8a50f465b1090
Sajjad Patel <sajjadp92@outlook.com>
2018-01-08 21:13:57 -0500

    This commit will have functions written in swift taking advantage of the language capabilities

---
+++ iOS11Programming/Functions.playground/Contents.swift
@@ -0,0 +1,52 @@
+//Void Return Type and Parameters
+
+func say1(_ s:String)-> Void{ print(s) }
+func say2(_ s:String)->(){ print(s) }
+func say3(_ s:String){ print(s) }
+
+//Functions without parameters
+func greet1()-> Void{ print("howdy") }
+func greet2()->(){ print("howdy") }
+func greet3(){ print("howdy") }
+
+//External Parameter Names
+func echoString(_ s:String, times:Int)->String{
+    var result = "";
+    for _ in 1...times {
+        result += s
+    }
+    return result
+}
+func echo(string s:String, times n:Int) -> String{
+    var result = ""
+    for _ in 1...n {
+        result += s
+    }
+    return result
+}
+
+//Overloading
+func say() -> String{ return "one" }
+func say() -> Int{ return 1 }
+
+func giveMeAString(_ s:String){
+    print("Thanks")
+}
+giveMeAString(say())
+let result1 = say() + " two"
+let result2 = say() + 1
+
+//Default Parameter Values
+class Dog{
+    func say(_ s:String, times:Int = 1){
+        for _ in 1...times{
+            print(s)
+        }
+    }
+}
+let d = Dog();
+d.say("Woof")
+d.say("Woof", times:3)
+
+//Variadic Parameters
+
